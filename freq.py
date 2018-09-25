87a3896b022ee1eb3d73ffb9ce0399c8287b40dc
Gabriel Melo <gvmgs@cin.ufpe.br>
2018-09-25 13:55:33 -0300

    Verificando a frequencia das palavras (bag of words), ainda tem que melhorar

--- freq.py
+++ freq.py
@@ -4,13 +4,28 @@ from sklearn.feature_extraction.text import CountVectorizer
 arqin = open("dataset/dataset.out").readlines()
 dataset = json.loads(arqin[0])
 print(len(dataset))
-
+txts = []
 word = []
 freq = {}
 
+'''
 txt = dataset[4]['content'].split("\n")
 print(type(txt))
-vectorizer = CountVectorizer()
 
-print( vectorizer.fit_transform(list(txt)).todense() )
-print( vectorizer.vocabulary_ )
+'''
+i = 0
+for elem in dataset:
+    aux = elem['content'].split("\n")
+    for t in aux:
+        txts.append(t)
+    i += 1
+    print(i)
+vectorizer = CountVectorizer()
+print(vectorizer.fit_transform(list(txts)).todense())
+print(vectorizer.vocabulary_)
+print(len(vectorizer.vocabulary_))
+
+arqout = open("dataset/test", 'w')
+
+for word in vectorizer.vocabulary_:
+    arqout.write("{}: {}\n".format(word, vectorizer.vocabulary_[word]))
