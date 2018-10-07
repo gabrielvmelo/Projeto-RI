920570e4bdfb66d711b1d6b22d907ccdfeaf7cf0
lmmc2 <lmmc2@cin.ufpe.com>
2018-10-07 16:49:56 -0300

    Otimizando classificador de paginas

--- crawler/src/main/kotlin/Main.kt
+++ crawler/src/main/kotlin/Main.kt
@@ -20,96 +20,118 @@ class Main {
                 "https://www.themoviedb.org",
                 "https://www.trakt.tv",
                 "https://www.tvguide.com",
-                "http://www.metacritic.com",
+                "https://www.metacritic.com",
                 "https://www.thetvdb.com",
                 "http://www.tv.com"
             )
             val pageClass = PageClassifier()
             val rbt = Robot()
-            print("Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/tv/the_sinner/s02")!!)}\n")
-            print("Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/tv/game_of_thrones?")!!)}\n")
-            print("Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/m/a_simple_favor")!!)}\n")
-            print("Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/")!!)}\n")
-            print("Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.imdb.com/registration/signin?u=https%3A%2F%2Fwww.imdb.com%2Flist%2Fwatchlist%3Fref_%3Dnv_wl_all_0")!!)}\n")
-            print("Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.themoviedb.org/movie/335983-venom?language=en-US")!!)}\n")
-            print("Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.themoviedb.org/tv/1402-the-walking-dead")!!)}\n")
-            print("Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://trakt.tv/shows/trending")!!)}\n")
+//            print("1 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/tv/the_sinner/s02")!!)}\n")
+//            print("2 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/tv/game_of_thrones?")!!)}\n")
+//            print("3 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.themoviedb.org/tv/1402-the-walking-dead")!!)}\n")
+//            print("4 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.imdb.com/title/tt0903747/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=12230b0e-0e00-43ed-9e59-8d5353703cce&pf_rd_r=X9XS2RYN19FPDXH72GTN&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=toptv&ref_=chttvtp_tt_5")!!)}\n")
+//            print("5 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://trakt.tv/shows/the-big-bang-theory")!!)}\n")
+//            print("6 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.metacritic.com/tv/the-walking-dead/season-9")!!)}\n")
+//            print("7 Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.thetvdb.com/series/stranger-things")!!)}\n")
+//            print("8 Relevante: ${pageClass.scorePage(rbt.downloadPage("http://www.tv.com/shows/the-dr-oz-show/")!!)}\n")
+//            print("9 Relevante: ${pageClass.scorePage(rbt.downloadPage("http://www.tv.com/shows/bleach/")!!)}\n")
+//            print("1 Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/m/a_simple_favor")!!)}\n")
+//            print("2 Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.themoviedb.org/movie/335983-venom?language=en-US")!!)}\n")
+//            print("3 Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.imdb.com/title/tt1270797/?ref_=inth_ov_tt")!!)}\n")
+//            print("4 Nao Relevante Game: ${pageClass.scorePage(rbt.downloadPage("https://www.metacritic.com/game/playstation-4/assassins-creed-odyssey")!!)}\n")
+//            print("5 Nao Relevante Movie: ${pageClass.scorePage(rbt.downloadPage("https://www.metacritic.com/movie/smallfoot")!!)}\n")
+//            print("1 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.rottentomatoes.com/")!!)}\n")
+//            print("2 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.imdb.com/registration/signin?u=https%3A%2F%2Fwww.imdb.com%2Flist%2Fwatchlist%3Fref_%3Dnv_wl_all_0")!!)}\n")
+//            print("3 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://trakt.tv/shows/trending")!!)}\n")
+//            print("4 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.themoviedb.org/discover/movie")!!)}\n")
+//            print("5 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://trakt.tv/calendars/shows")!!)}\n")
+//            print("6 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.tvguide.com/news/")!!)}\n")
+//            print("7 Nao Relevante: ${pageClass.scorePage(rbt.downloadPage("https://www.thetvdb.com/series/supergirl/seasons/1")!!)}\n")
 
-//
-//            //Salvar robots.txt
-//            val mapRobots = HashMap<String, LinkedList<String>>()
-//
-//            //Passar pelos 8 sites e coletar 1000 links de cada
-//            for(item in URLs){
-//                val fronteira = Frontier()
-//                fronteira.addURL(item)
-//
-//                val domain = getDomainName(item)
-//                print("Domain: $domain\n")
-//
-//                val robots = CheckRobots()
-//                if(!mapRobots.contains(item)) mapRobots[item] = robots.readTxt(item+"robots.txt")
-//
-//                var contador = 0
-//
-//                while (!fronteira.vazia() && contador < 1000){
-//                    val url = fronteira.remove()
-//                    print("URL:$url\n")
-//
-//                    //Analisar se link esta no dominio
-//
-//                    if(domain != getDomainName(url)){
-//                        continue
-//                    }
-//
-//                    //Analisar robots.txt
-//                    if(checkRules(mapRobots[item]!!, url)){
-//                        print("Entrou checkRules")
-//                        continue
-//                    }
-//
-//                    //Analisar se eh uma pagina html e fazer download da pagina se for
-//                    val rbt = Robot()
-//                    var html: String
-//                    try {
-//                        val ishtml = rbt.downloadPage(url)
-//                        if (ishtml != null){
-//                            html = ishtml
-//                        } else {
-//                            print("Nao eh html\n")
-//                            continue
-//                        }
-//                    } catch (e: Exception) {
-//                        print("Nao esta nos tipos permitidos\n")
-//                        continue
-//                    }
-//
-//                    //Analisar se pagina ja foi visitada previamente
-//                    val dirName = domain.substringBefore(".")
-//                    val formattedURL = formatURL(url)
-//                    val repo = PageRepository(dirName, formattedURL)
-//
-//                    if (repo.checkPage()){
-//                        continue
-//                    }
-//
-//                    //Armazena pagina visitada
-//                    repo.storePage(html)
-//
-//                    //Passar pagina pelo parser para pegar links existentes
-//                    val txtProcessor = TextProcessor()
-//                    val links = txtProcessor.getLinks(html, item)
-//                    print("links: " + links + " ")
-//
-//                    //Atualiza fronteira
-//                    for (link in links){
-//                        fronteira.addURL(link)
-//                    }
-//                    contador += 1
-//                    print("\n" + contador + "\n")
-//                }
-//
-//            }
+
+            //Salvar robots.txt
+            val mapRobots = HashMap<String, LinkedList<String>>()
+
+            //Salvar links e seus scores
+            val urlScoreMap = hashMapOf<String, Int>()
+
+            //Passar pelos 8 sites e coletar 1000 links de cada
+            for(item in URLs){
+                val fronteira = Frontier()
+                fronteira.addURL(item)
+
+                val domain = getDomainName(item)
+                print("Domain: $domain\n")
+
+                val robots = CheckRobots()
+                if(!mapRobots.contains(item)) mapRobots[item] = robots.readTxt(item+"robots.txt")
+
+                var contador = 0
+
+                while (!fronteira.vazia() && contador < 25){
+                    val url = fronteira.remove()
+                    print("URL:$url\n")
+
+                    //Analisar se link esta no dominio
+
+                    if(domain != getDomainName(url)){
+                        continue
+                    }
+
+                    //Analisar robots.txt
+                    if(checkRules(mapRobots[item]!!, url)){
+                        print("Entrou checkRules")
+                        continue
+                    }
+
+                    //Analisar se eh uma pagina html e fazer download da pagina se for
+                    val rbt = Robot()
+                    var html: String
+                    try {
+                        val ishtml = rbt.downloadPage(url)
+                        if (ishtml != null){
+                            html = ishtml
+                        } else {
+                            println("Nao eh html\n")
+                            continue
+                        }
+                    } catch (e: Exception) {
+                        println(e.message)
+                        continue
+                    }
+
+                    //Analisar se pagina ja foi visitada previamente
+                    val dirName = domain.substringBefore(".")
+                    val formattedURL = formatURL(url)
+                    val repo = PageRepository(dirName, formattedURL)
+
+                    if (repo.checkPage()){
+                        continue
+                    }
+
+                    //Armazena pagina visitada
+                    repo.storePage(html)
+
+                    //Calcula score e salva no map
+                    val pageClassifier = PageClassifier()
+                    val score = pageClassifier.scorePage(html)
+                    urlScoreMap[url] = score
+
+                    //Passar pagina pelo parser para pegar links existentes
+                    val txtProcessor = TextProcessor()
+                    val links = txtProcessor.getLinks(html, item)
+                    println("links: " + links + " ")
+
+                    //Atualiza fronteira
+                    for (link in links){
+                        fronteira.addURL(link)
+                    }
+                    contador += 1
+                    println("\n" + contador + "\n")
+                }
+
+            }
+//            println(urlScoreMap)
 
         }
 
--- crawler/src/main/kotlin/PageClassifier.kt
+++ crawler/src/main/kotlin/PageClassifier.kt
@@ -2,17 +2,50 @@ import org.jsoup.Jsoup
 
 //Analisando pagina para definir se eh relevante ou nao a partir das palavras presentes
 class PageClassifier {
+    val hashMap = hashMapOf(
+            "season" to 10, "seasons" to 10, "tv" to 5, "episodes" to 10, "series" to 10,"serie" to 10, "episode" to 10, "guide" to 5, "status" to 7, "returning" to 7,
+            "ended" to 7, "show" to 10, "continuing" to 7, "info" to 3, "genre" to 3, "network" to 3, "premiere" to 3, "date" to 3, "exec" to 3,
+            "executive" to 3, "producers" to 3, "critic" to 3, "features" to 3, "stars" to 3, "watchers" to 3, "plays" to 3, "rating" to 3, "reviews" to 3,
+            "fresh" to 3, "rotten" to 3, "tomatometer" to 3, "critics" to 3, "audience" to 3, "score" to 3, "ratings" to 3, "videos" to 3, "photos" to 3,
+            "cast" to 3, "netflix" to 1, "creators" to 1, "starring" to 1, "details" to 1, "trailers" to 1, "pictures" to 1,
+            "creator" to 1, "crew" to 1, "popularity" to 1, "storyline" to 1, "plot" to 1, "genres" to 1, "taglines" to 1, "certificate" to 1, "parents" to 1,
+            "country" to 1, "language" to 1, "filming" to 1, "locations" to 1, "company" to 1, "credits" to 1, "technical" to 1, "specs" to 1, "sound" to 1,
+            "color" to 1, "runtime" to 1, "quotes" to 1, "soundtracks" to 1, "user" to 1, "overview" to 1, "featured" to 1, "play" to 1, "facts" to 1,
+            "type" to 1, "keywords" to 1, "media" to 1, "current" to 1, "released" to 1, "collected" to 1, "comments" to 1, "lists" to 1, "votes" to 1,
+            "airs" to 1, "premieres" to 1, "recent" to 1, "watchlist" to 1, "view" to 1, "premise" to 1, "full" to 1, "summary" to 1, "genre(s)" to 1, "credits" to 1,
+            "director" to 1, "awards" to 1, "positive" to 1, "mixed" to 1, "negative" to 1, "imdb" to 1, "thetvdb" to 1, "posters" to 1, "banners" to 1,
+            "film" to (-10), "tickets" to (-10), "showtimes" to (-10), "movie" to (-7), "movies" to (-7), "theaters" to (-10), "dvd" to (-10), "albums" to (-10), "record" to (-5),
+            "cheats" to (-5), "controllers" to (-5), "developer" to (-5), "playstation" to (-10), "log" to (-5), "account" to (-5), "donate" to (-5), "interviews" to (-1),
+            "sponsors" to (-5), "supporting" to (-5), "community" to (-5), "feed" to (-5), "near" to (-5), "sign" to (-5), "news" to (-1), "new" to (-1),
+            "login" to (-5), "member" to (-5), "developers" to (-5), "blu" to (-10), "ray" to (-10), "calendar" to (-5), "football" to (-5), "league" to (-5),
+            "sports" to (-5), "studio" to (-3), "label" to (-3), "platform" to (-3), "players" to (-3), "apps" to (-3), "devices" to (-3), "report" to (-3),
+            "budget" to (-1), "revenue" to (-1), "category" to (-1), "channels" to (-1), "listings" to (-1), "people" to (-1), "showing" to (-1), "sorted" to (-1)
+    )
 
     fun scorePage(html: String): Int{
+        val scoresMap = hashMapOf<String, Int>()
+        val tokens = pageTokenizer(html).map { token -> token.toLowerCase() }
 
-        val listScores = arrayListOf<Int>()
-        val tokens = pageTokenizer(html)
+        val wordLimitMap = hashMapOf(
+                "tv" to 100, "channel" to 50, "network" to 15, "rate" to 15, "watchlist" to 30, "watch" to 25, "released" to 100, "episode" to 75, "episodes" to 75,
+                "guide" to 30, "season" to 60, "date" to 60, "genres" to 50, "runtime" to 50
+        )
+
         for (token in tokens){
-            listScores.add(scoreToken(token.toLowerCase()))
+            val value = scoreToken(token)
+            if (value == 0) continue
+            if (!scoresMap.contains(token)) scoresMap[token] = value
+            else {
+                scoresMap[token] = scoresMap[token]!! + value
+            }
+        }
+
+        for(word in wordLimitMap.keys){
+            if (scoresMap.contains(word) && (scoresMap[word]!!/hashMap[word]!!) > wordLimitMap[word]!!) scoresMap[word] = -2000
         }
 
         var score = 0
-        for (value in listScores){
+        for (value in scoresMap.values){
             score += value
         }
 
@@ -21,32 +54,13 @@ class PageClassifier {
 
     private fun pageTokenizer(html: String): List<String>{
         val textHTML = Jsoup.parse(html).text()
-        println(textHTML)
         val regex = ":|/|\\.|-|_|&| |;|,|=|\\?".toRegex()
         return textHTML.split(regex)
     }
 
     private fun scoreToken(token: String): Int{
         var score = 0
-        val hashMap = hashMapOf(
-                "season" to 5, "seasons" to 5, "tv" to 5, "episodes" to 5, "series" to 5, "episode" to 5, "guide" to 5, "status" to 5, "returning" to 5,
-                "ended" to 5, "show" to 5, "continuing" to 5, "info" to 3, "genre" to 3, "network" to 3, "premiere" to 3, "date" to 3, "exec" to 3,
-                "executive" to 3, "producers" to 3, "critic" to 3, "features" to 3, "stars" to 3, "watchers" to 3, "plays" to 3, "rating" to 3, "reviews" to 3,
-                "fresh" to 3, "rotten" to 3, "tomatometer" to 3, "critics" to 3, "audience" to 3, "score" to 3, "ratings" to 3, "videos" to 3, "photos" to 3,
-                "cast" to 3, "netflix" to 3, "news" to 1, "interviews" to 1, "creators" to 1, "starring" to 1, "details" to 1, "trailers" to 1, "pictures" to 1,
-                "creator" to 1, "crew" to 1, "popularity" to 1, "storyline" to 1, "plot" to 1, "genres" to 1, "taglines" to 1, "certificate" to 1, "parents" to 1,
-                "country" to 1, "language" to 1, "filming" to 1, "locations" to 1, "company" to 1, "credits" to 1, "technical" to 1, "specs" to 1, "sound" to 1,
-                "color" to 1, "runtime" to 1, "quotes" to 1, "soundtracks" to 1, "user" to 1, "overview" to 1, "featured" to 1, "play" to 1, "facts" to 1,
-                "type" to 1, "keywords" to 1, "media" to 1, "current" to 1, "released" to 1, "collected" to 1, "comments" to 1, "lists" to 1, "votes" to 1,
-                "airs" to 1, "premieres" to 1, "recent" to 1, "watchlist" to 1, "premise" to 1, "full" to 1, "summary" to 1, "genre(s)" to 1, "credits" to 1,
-                "director" to 1, "awards" to 1, "positive" to 1, "mixed" to 1, "negative" to 1, "imdb" to 1, "posters" to 1, "banners" to 1,
-                "tickets" to (-5), "showtimes" to (-5), "movie" to (-5), "theaters" to (-5), "dvd" to (-5), "albums" to (-5), "record" to (-5),
-                "cheats" to (-5), "controllers" to (-5), "developer" to (-5), "playstation" to (-5), "log" to (-5), "account" to (-5), "donate" to (-5),
-                "sponsors" to (-5), "supporting" to (-5), "community" to (-5), "feed" to (-5), "tickets" to (-5), "showtimes" to (-5), "near" to (-5), "sign" to (-5),
-                "login" to (-5), "member" to (-5), "developers" to (-5), "blu" to (-5), "ray" to (-5), "calendar" to (-5), "football" to (-5), "league" to (-5),
-                "sports" to (-5), "studio" to (-3), "label" to (-3), "platform" to (-3), "players" to (-3), "apps" to (-3), "devices" to (-3), "report" to (-3),
-                "budget" to (-1), "revenue" to (-1), "category" to (-1), "channels" to (-1), "listings" to (-1), "people" to (-1), "showing" to (-1), "sorted" to (-1)
-        )
+
         if (hashMap.contains(token)){
             score = hashMap.getValue(token)
         }
