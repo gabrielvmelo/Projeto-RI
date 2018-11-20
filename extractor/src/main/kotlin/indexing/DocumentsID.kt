e5dd1186ada635634222c507d65774f54391229f
lmmc2 <lmmc2@cin.ufpe.br>
2018-11-20 08:47:56 -0300

    IDs maiores

--- extractor/src/main/kotlin/indexing/DocumentsID.kt
+++ extractor/src/main/kotlin/indexing/DocumentsID.kt
@@ -1,12 +1,15 @@
 package indexing
 
+import java.util.*
+
 //criacao dos IDs dos documentos(paginas)
 class DocumentsID {
     var documentsIDs = hashMapOf<String, Int>()
 
     fun createIDs(URLs: Array<String>): HashMap<String, Int>{
+        val random = Random().nextInt(10000000-1000000) + 1000000
         for((counter, URL) in URLs.withIndex()){
-            if(!documentsIDs.contains(URL)) documentsIDs[URL] = counter
+            if(!documentsIDs.contains(URL)) documentsIDs[URL] = counter + random
         }
 
         return documentsIDs
